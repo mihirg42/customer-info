cf31166e0b12a50c2e07ee36edf0bbf7e85b88b1
mihirg42 <guptamihir7@gmail.com>
2026-03-07 13:35:46 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace customer_info.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
